861401e7ca7bb1fc8ad370bfdca1749fa90b2d56
gouravchauhan712 <117297226+gouravchauhan712@users.noreply.github.com>
2025-03-23 15:12:35 +0530

    Update sms app.py

--- sms app.py
+++ sms app.py
@@ -1,16 +1,14 @@
 import nltk
-nltk.download('punkt')  # Download the tokenizer
-nltk.download('stopwords')  # Ensure stopwords are also downloaded
+nltk.download('punkt')  # Download tokenizer
+nltk.download('stopwords')  # Download stopwords
 
 import streamlit as st
 import pickle
 import string
 from nltk.corpus import stopwords
-import nltk
 from nltk.stem.porter import PorterStemmer
 
 ps = PorterStemmer()
-
 
 def transform_text(text):
     text = text.lower()
@@ -36,23 +34,20 @@ def transform_text(text):
 
     return " ".join(y)
 
-tfidf = pickle.load(open('vectorizer.pkl','rb'))
-model = pickle.load(open('model.pkl','rb'))
+# Load vectorizer and model
+tfidf = pickle.load(open('vectorizer.pkl', 'rb'))
+model = pickle.load(open('model.pkl', 'rb'))
 
 st.title("Email/SMS Spam Classifier")
 
 input_sms = st.text_area("Enter the message")
 
 if st.button('Predict'):
-
-    # 1. preprocess
+    # 1. Preprocess
     transformed_sms = transform_text(input_sms)
-    # 2. vectorize
+    # 2. Vectorize
     vector_input = tfidf.transform([transformed_sms])
-    # 3. predict
+    # 3. Predict
     result = model.predict(vector_input)[0]
-    # 4. Display
-    if result == 1:
-        st.header("Spam")
-    else:
-        st.header("Not Spam")
+    # 4. Display Result
+    st.header("Spam" if result == 1 else "Not Spam")
